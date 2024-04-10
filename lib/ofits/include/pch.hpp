d15e89d2630780103e750864084bad18176b7699
cilantrofe <131597495+cilantrofe@users.noreply.github.com>
2024-04-10 15:01:12 +0300

    Add comments and documentation

---
+++ lib/ofits/include/pch.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file pch.hpp
+ * @author Alina Gubeeva
+ * @brief Included headers
+ * @version 0.1
+ * @date 2024-04-10
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+// STL
+#include <string>
+#include <tuple>
+#include <optional>
+#include <array>
+#include <cmath>
+
+// Boost
+#include <boost/asio.hpp>
+#include <boost/asio/write_at.hpp>
